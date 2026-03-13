d1ac6e6436c955e9133d2ab29907ed93603ccbb9
Aruna.M <arunakini2004@gmail.com>
2026-03-13 11:58:41 +0530

    Create stack.java

---
+++ java/stack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNext()) {
+            String input = sc.next();
+            System.out.println(isBalanced(input));
+        }
+    }
+    
+    static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '(' || current == '[' || current == '{') {
+                stack.push(current);
+            } else {
+                if (stack.isEmpty()) return false;
+                
+                char top = stack.pop();
+                
+                if (current == ')' && top != '(') return false;
+                if (current == ']' && top != '[') return false;
+                if (current == '}' && top != '{') return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
+
+
+
